a4a207674e57926d5c2bdd674c02bcf3314eefda
LiS2Lim <s2alua.fu@gmail.com>
2023-06-22 09:31:19 +0900

    Study about Inner class & Init String

--- src/Study/Study.java
+++ src/Study/Study.java
@@ -1,13 +1,55 @@
 package Study;
 
 public class Study {
-    public static void main(String[] args) {
-        int[] list = new int[10];
-        try {
-            System.out.println(list[10]);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            e.printStackTrace();
+
+    class Inner extends Study{
+
+        //field
+        private String name;
+
+        //Constructor
+        Inner(String name) {
+            this.name = name;
         }
+
+        //Method
+        public String getName() {
+            return this.name;
+        }
+
+        @Override
+        public void tst() {
+            System.out.println("tst by inner");
+        }
+    }
+    public static void main(String[] args) {
+        Inner n = new Study().new Inner("James");
+        n.tst();
+
+        Study s = n.new Inner("test");
+        n.tst();
+    }
+
+    //Method
+    public void tst() {
+        System.out.println("tst");
+    }
+}
+
+class InitString {
+    static class Inner {
+        String name;
+    }
+    public static void main(String[] args) {
+        Inner n = new Inner();
+        String one = null;
+        String two = "null";
+        String three = null;
+        String four = "null";
+        String five = new String("null");
+
+        System.out.println(two == four);
+        System.out.println(two == five);
+        System.out.println(one == n.name);
     }
 }
